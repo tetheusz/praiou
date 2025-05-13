5f1fa9aad07c4c4685d4889b99048ae5acd4d84a
tetheusz <mhprocopio372@gmail.com>
2025-05-13 14:58:20 -0300

    first commit

---
+++ morungatas-main/src/js/script.js
@@ -0,0 +1,27 @@
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("slow")
+})
+
+
+const newYear = new Date('may 17, 2025').getTime();
+let days, hours, minutes, seconds
+let regressCount = document.getElementById("regressCount")
+
+setInterval(function () {
+  let dateThisMoment = new Date().getTime()
+  let secondsThisMoment = (newYear - dateThisMoment) / 1000;
+
+  days = parseInt(secondsThisMoment / 86400)
+  secondsThisMoment = secondsThisMoment % 86400
+
+  hours = parseInt(secondsThisMoment / 3600)
+  secondsThisMoment = secondsThisMoment % 3600
+
+  minutes = parseInt(secondsThisMoment / 60)
+  seconds = parseInt(secondsThisMoment % 60)
+
+  document.getElementById('day').innerHTML = days
+  document.getElementById('hour').innerHTML = hours
+  document.getElementById('minute').innerHTML = minutes
+  document.getElementById('second').innerHTML = seconds
+}, 1000)
